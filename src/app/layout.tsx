7b56e722ddf7b4c05da9383ed5fe851747677495
alexkjzz <alex.lam@orange.fr>
2025-05-06 11:13:15 +0200

    feat: added layout style and compo

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,9 @@
 import type { Metadata } from "next";
 import "@/styles/globals.css";
-
+import Footer from "@/components/Footer";
+import Header from "@/components/Header";
+import Sidebar from "@/components/Sidebar";
+import Terminal from "@/components/Terminal";
 
 export const metadata: Metadata = {
   title: "WeScrap.mp3",
@@ -9,16 +12,33 @@ export const metadata: Metadata = {
 
 export default function RootLayout({
   children,
-}: Readonly<{
+}: {
   children: React.ReactNode;
-}>) {
+}) {
   return (
     <html lang="en">
-      <body
-        className= "antialiased"
-      >
-        {children}
+      <body className="min-h-screen bg-[#121212] text-white overflow-hidden">
+        <Header />
+
+        <Sidebar />
+
+        <div className="ml-[120px] mt-24 mb-24 h-[calc(100vh-96px-96px)] flex gap-4">
+          <main className="flex-[0.65] h-full overflow-auto">
+            <div className="bg-[#1e1e1e] rounded-xl shadow-lg p-8 w-full h-full">
+              {children}
+            </div>
+          </main>
+
+          <section className="flex-[0.35] h-full overflow-auto">
+            <div className="bg-[#1e1e1e] rounded-xl shadow-lg p-8 w-full h-full">
+              <Terminal />
+            </div>
+          </section>
+        </div>
+
+        <Footer />
       </body>
     </html>
   );
 }
+
---
+++ src/components/Terminal.tsx
@@ -0,0 +1,8 @@
+export default function Terminal() {
+  return (
+    <div className="bg-[#1e1e1e] rounded-xl shadow-lg p-8 w-full h-full">
+      <h2 className="text-white text-lg">Terminal</h2>
+      <p className="text-white text-sm mt-4">Bienvenue dans le terminal !</p>
+    </div>
+  );
+}
